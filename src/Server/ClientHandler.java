3ccaf67d4d5607173381e6a9ceabb3ea627950a3
Michael V. Gritsenko <payterms@mail.ru>
2019-03-13 18:18:33 +0300

    реализовано управление потоками через ExecutorService.

--- src/Server/ClientHandler.java
+++ src/Server/ClientHandler.java
@@ -1,7 +1,5 @@
 package Server;
 
-import Server.auth.AuthServiceImpl;
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -11,7 +9,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-public class ClientHandler {
+public class ClientHandler implements Runnable{
 
     private static final Pattern MESSAGE_PATTERN = Pattern.compile("^/w (\\w+) (.+)", Pattern.MULTILINE);
     private static final Pattern USER_UPDATE_PATTERN = Pattern.compile("^/userupdate (\\w+) (\\w+)$");
@@ -33,60 +31,60 @@ public class ClientHandler {
         this.server = server;
         this.inp = new DataInputStream(socket.getInputStream());
         this.out = new DataOutputStream(socket.getOutputStream());
+        this.handleThread = new Thread(this);
 
-        this.handleThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    while (!Thread.currentThread().isInterrupted()) {
-                        String msg = inp.readUTF();
-                        System.out.printf("Message from user %s: %s%n", ClientHandler.this.username, msg);
+        handleThread.start();
+    }
 
-                        Matcher matcher = MESSAGE_PATTERN.matcher(msg);
-                        if (matcher.matches()) {
-                            String userTo = matcher.group(1);
-                            String message = matcher.group(2);
-                            server.sendMessage(userTo, ClientHandler.this.username, message);
-                        } else if (msg.startsWith(USER_LIST_PATTERN)) {
-                            List<String> usrList = server.getUserList();
-                            String msgToSend = USER_LIST_PATTERN;
-                            for (int i = 0; i < usrList.size(); i++) {
-                                msgToSend += " " + usrList.get(i);
-                            }
-                            System.out.printf("Sending user list to user %s: %s%n", ClientHandler.this.username, msgToSend);
-                            out.writeUTF(msgToSend);
-                        } else if (msg.startsWith(UPDATE_PATTERN)) {
-                            matcher = USER_UPDATE_PATTERN.matcher(msg);
-                            if (matcher.matches()) {
-                                String oldUsername = matcher.group(1);
-                                String newUsername = matcher.group(2);
-                                if (server.updateUsername(oldUsername, newUsername)) {
-                                    out.writeUTF("/upd successful");
-                                    out.flush();
-                                    server.broadcastUserUpdate(oldUsername, newUsername);
-                                    ClientHandler.this.setUsername(newUsername);
+    @Override
+    public void run() {
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                String msg = inp.readUTF();
+                System.out.printf("Message from user %s: %s%n", ClientHandler.this.username, msg);
 
-                                }else{
-                                    out.writeUTF("/upd failed");
-                                    out.flush();
-                                }
-                            }
+                Matcher matcher = MESSAGE_PATTERN.matcher(msg);
+                if (matcher.matches()) {
+                    String userTo = matcher.group(1);
+                    String message = matcher.group(2);
+                    server.sendMessage(userTo, ClientHandler.this.username, message);
+                } else if (msg.startsWith(USER_LIST_PATTERN)) {
+                    List<String> usrList = server.getUserList();
+                    String msgToSend = USER_LIST_PATTERN;
+                    for (int i = 0; i < usrList.size(); i++) {
+                        msgToSend += " " + usrList.get(i);
+                    }
+                    System.out.printf("Sending user list to user %s: %s%n", ClientHandler.this.username, msgToSend);
+                    out.writeUTF(msgToSend);
+                } else if (msg.startsWith(UPDATE_PATTERN)) {
+                    matcher = USER_UPDATE_PATTERN.matcher(msg);
+                    if (matcher.matches()) {
+                        String oldUsername = matcher.group(1);
+                        String newUsername = matcher.group(2);
+                        if (server.updateUsername(oldUsername, newUsername)) {
+                            out.writeUTF("/upd successful");
+                            out.flush();
+                            server.broadcastUserUpdate(oldUsername, newUsername);
+                            ClientHandler.this.setUsername(newUsername);
+
+                        }else{
+                            out.writeUTF("/upd failed");
+                            out.flush();
                         }
                     }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    System.out.printf("Client %s disconnected%n", username);
-                    try {
-                        socket.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    server.unsubscribeClient(ClientHandler.this);
                 }
             }
-        });
-        handleThread.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.printf("Client %s disconnected%n", username);
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            server.unsubscribeClient(ClientHandler.this);
+        }
     }
 
     public void sendMessage(String userFrom, String msg) throws IOException {
---
+++ src/client/WaitNotifyClass.java
@@ -0,0 +1,79 @@
+package client;
+
+/*
+    Создать три потока, каждый из которых выводит определенную букву (A, B и C) 5 раз (порядок – ABСABСABС).
+    Используйте wait/notify/notifyAll.
+*/
+
+public class WaitNotifyClass {
+    private final Object mon = new Object();
+    private volatile char currentLetter = 'A';
+    private final int CYCLE_COUNT = 5;
+
+    public static void main(String[] args) {
+        WaitNotifyClass w = new WaitNotifyClass();
+        Thread t1 = new Thread(() -> {
+            w.printA();
+        });
+        Thread t2 = new Thread(() -> {
+            w.printB();
+        });
+        Thread t3 = new Thread(() -> {
+            w.printC();
+        });
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+
+    public void printA() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < CYCLE_COUNT; i++) {
+                    while (currentLetter != 'A') {
+                        mon.wait();
+                    }
+                    System.out.print("A");
+                    currentLetter = 'B';
+                    mon.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printB() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < CYCLE_COUNT; i++) {
+                    while (currentLetter != 'B') {
+                        mon.wait();
+                    }
+                    System.out.print("B");
+                    currentLetter = 'C';
+                    mon.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printC() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < CYCLE_COUNT; i++) {
+                    while (currentLetter != 'C') {
+                        mon.wait();
+                    }
+                    System.out.print("C");
+                    currentLetter = 'A';
+                    mon.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
